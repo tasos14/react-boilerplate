77d06a72ca358f9f5f3e25607e52ce5adec8c5f9
Tasos <atheodoridis@trigonongroup.com>
2019-09-09 21:55:23 +0300

    mobx for state managment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'mobx-react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'components/app/app.react';
 
-render(<App />, document.getElementById('app'));
+import user from 'stores/user';
+
+const store = { user };
+
+if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+}
+
+render(
+    <Router>
+        <Provider {...store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById('app')
+);
 
 if (process.env.NODE_ENV !== 'production') {
     module.hot.accept();
