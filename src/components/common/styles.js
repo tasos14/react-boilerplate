40baa51f0c32c8ce6b541d321a9006f15ee81ae4
Thomas <tliakos@trigonongroup.com>
2019-10-23 15:48:48 +0300

    change colors

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const H2 = styled.h2`
-    background-color: pink;
-    color: purple;
+    background-color: red;
+    color: green;
 `;
 
 export const H3 = styled.h3`
@@ -10,5 +10,5 @@ export const H3 = styled.h3`
 `;
 
 export const Button = styled.button`
-    background-color: #eca7cd;
+    background-color: #78cdee;
 `;
